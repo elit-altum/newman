3c056c9adc5db0d2c684a6ea9dd961cf86e74621
elit-altum <manan.sharma311@gmail.com>
2021-04-07 18:40:27 +0530

    add: JWT for server daemon auth

--- bin/subberClient.js
+++ bin/subberClient.js
@@ -1,71 +1,72 @@
 /* eslint-disable */
 // communicate to the newman dashboard socket server
-let emitProcessEnd = () => {};
-let listenEvents = () => {};
-let emitProcessStart = () => {};
+// client library to connect to socket server
+const io = require("socket.io-client");
+const jwt = require('jsonwebtoken');
 
-try {
-  // client library to connect to socket server
-  const io = require("socket.io-client");
+// unique process id
+const processId = `run-${Date.now().toString()}`;
 
-  // unique process id
-  const processId = `run-${Date.now().toString()}`;
+// connect to the socket server as a client
+const socket = io("http://localhost:5001/", {
+  auth: {
+    token: jwt.sign({processId}, "insertSecretKeyHere")
+  }
+});
 
-  // connect to the socket server as a client
-  const socket = io("http://localhost:5001/");
+// client-side
+socket.on("connect_error", (err) => {
+    console.log(err.message);
+});
 
-  // to notify server about start of a new process
-  emitProcessStart = (argv) => {
-    let cmdString = '';
-    let runIndex = argv.indexOf('run');
+// to notify server about start of a new process
+const emitProcessStart = (argv) => {
+  let cmdString = '';
+  let runIndex = argv.indexOf('run');
 
-    // get the user command string from the argv
-    if(runIndex !== -1) {
-      for( i = runIndex; i < argv.length; ++i) {
-        cmdString += argv[i] + " ";
-      }
+  // get the user command string from the argv
+  if(runIndex !== -1) {
+    for( i = runIndex; i < argv.length; ++i) {
+      cmdString += argv[i] + " ";
     }
+  }
 
-    socket.emit("newProcess", {
+  socket.emit("newProcess", {
+      processId,
+      startTime: new Date(),
+      command: cmdString
+  });
+}
+
+// to notify server about the end of a process
+const emitProcessEnd = () => {
+    socket.emit("endProcess", {
         processId,
-        startTime: new Date(),
-        command: cmdString
     });
-  }
+    
+    // close the client connection with socket
+    socket.close();
+};
 
-  // to notify server about the end of a process
-  emitProcessEnd = () => {
-      socket.emit("endProcess", {
-          processId,
-      });
-      
-      // close the client connection with socket
-      socket.close();
-  };
+// listen for various control events from the server
+const listenEvents = () => {
+  socket.on('pauseProcess', (data) => {
+    if (data.processId === processId) {
+      console.log("\nPAUSE PROCESS\n");
+    }
+  });
 
-  // listen for various control events from the server
-  listenEvents = () => {
-    socket.on('pauseProcess', (data) => {
+  socket.on("resumeProcess", (data) => {
       if (data.processId === processId) {
-        console.log("\nPAUSE PROCESS\n");
+          console.log("\nRESUME PROCESS\n");
       }
-    });
+  });
 
-    socket.on("resumeProcess", (data) => {
-        if (data.processId === processId) {
-            console.log("\nRESUME PROCESS\n");
-        }
-    });
-
-    socket.on("abortProcess", (data) => {
-        if (data.processId === processId) {
-            console.log("\nABORT PROCESS\n");
-        }
-    });
-  }
-
-} catch (error) {
-  // Empty catch if the dashboard is not running
+  socket.on("abortProcess", (data) => {
+      if (data.processId === processId) {
+          console.log("\nABORT PROCESS\n");
+      }
+  });
 }
 
 module.exports = {
